c5f480436572f3c4f5de49d39378b5e3783b5239
Krishantha Dinesh <krishantha@krishantha.com>
2021-11-21 01:23:02 +0530

    update pagination

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { skip } from 'rxjs';
 import { OrderService } from './order.service';
 
 @Controller('orders')
@@ -6,7 +7,15 @@ export class OrderController {
   constructor(private orderService: OrderService) {}
 
   @Get('/:category')
-  async fetchOrders(@Param('category') category: string) {
-    return await this.orderService.getAllOrders(category);
+  async fetchOrders(
+    @Param('category') category: string,
+    @Query('skip') skip: string,
+    @Query('take') take: string,
+  ) {
+    return await this.orderService.getAllOrders(
+      category,
+      parseInt(skip),
+      parseInt(take),
+    );
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -7,10 +7,14 @@ import { UtilService } from 'src/util/util.service';
 export class OrderService {
   constructor(private http: HttpclientService, private cache: UtilService) {}
 
-  async getAllOrders(category: string) {
+  async getAllOrders(category: string, skip: number, take: number) {
     const key = `OrderService:orders:group:${category}`;
 
-    const cached = await this.cache.readListFromCache(key, 0, -1);
+    const cached = await this.cache.readListFromCache(
+      key,
+      skip,
+      take + skip - 1,
+    );
     if (cached && cached.length > 0) {
       console.log('cache found');
       return cached.map((element) => JSON.parse(element));
@@ -18,7 +22,7 @@ export class OrderService {
       console.log('cache missed');
       const data = await this.http.getAllOreders();
       const groupedData = this.groupData(data);
-      await this.cache.cacheList(key, groupedData, 60);
+      await this.cache.cacheList(key, groupedData, 600);
       console.log('cached success');
       return groupedData;
     }
